4c4056b349af19aa422ad691c1a448c26141bbcf
Dany Pineault <ctio@Danys-MacBook-Pro.local>
2022-12-24 11:49:52 -0500

    test

--- src/routes/tournaments/+page.svelte
+++ src/routes/tournaments/+page.svelte
@@ -9,7 +9,7 @@
 
 <div class="bg-blue-dark py-8 px-8 rounded-2xl">
 	<div class="flex felx-row justify-between py-4 px-8 ">
-		<h1 class="text-2xl">Tournaments</h1>
+		<h1 class="text-2xl">Your Tournaments</h1>
 		<a href="/tournaments/create" class="bg-blue-accent px-2 py-2">create a new</a>
 	</div>
 	<div class="flex flex-col gap-4">
